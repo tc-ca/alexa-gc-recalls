f9a9feaea01bc0079e1f070ac2fc4272765f930a
Ainsley Bernard <ains_bernard@hotmail.com>
2019-01-30 16:19:53 -0500

    Updated constants

--- lambda/custom/Constants.js
+++ lambda/custom/Constants.js
@@ -12,22 +12,45 @@ module.exports = Object.freeze({
     RequestingNextRecallInfo: 2,
     RestartRecallSearch: 3,
     ResolvingAmbiguousModel: 4,
+
     ResponsedYesToWantingToReceiveSMS: 5,
-    ResponsedYesToCorrectPhoneNumberFoundOnAccount: 6
+    ResponsedNoToWantingToReceiveSMS: 6,
+
+    ResponsedYesToCorrectPhoneNumberFoundOnAccount: 7,
+    ResponsedNoToCorrectPhoneNumberFoundOnAccount: 8,
+
+    ResponsedYesToWantingRecallRead: 9,
+    ResponsedNoToWantingRecallRead: 10,
+
+    RespondedYesToRepeatRecallInfo: 11,
+    RespondedNoToRepeatRecallInfo: 12
 
   },
 
   SearchVehicleRecallIntentYesNoQuestions: {
     WouldYouLikeToSearchForAnotherRecall: 0,
     WouldYouLikeToHearTheNextRecall: 1,
-    ProvideComfirmationOfAmbiguousModel: 2,
+    IsItModelAOrModelB: 2,
     IsYourPhoneNumberFiveFiveFiveBlahBlah: 4,
-    WouldYouLikeToRecieveSMSMessage: 5
+    WouldYouLikeToRecieveSMSMessage: 5,
+    WouldYouLikeToMeReadTheRecall: 6,
+    WouldYouLikeTheRecallInformationRepeated: 7
+
   },
 
   VehicleConversationContext: {
     RecallInfo: 0,
-    AmbiguousModel: 1
+    AmbiguousModel: 1,
+    SearchResultFindings: 2
+  },
+
+  VehicleSearchFindings: {
+    SearchNotConducted: -1,
+    NoRecallsFound: 0,
+    SingleRecallFound: 1,
+    MultipleRecallsFound: 2,
+    NonValidModelFound: 3,
+    AmbigiousModelFound: 4
   },
 
   sessionKeys: {
