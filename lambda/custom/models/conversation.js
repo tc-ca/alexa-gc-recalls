413c55799cd4c484292222d3422ea4f3032b8f96
Ainsley Bernard <ains_bernard@hotmail.com>
2019-01-30 16:26:40 -0500

    Update conversation.js

--- lambda/custom/models/conversation.js
+++ lambda/custom/models/conversation.js
@@ -3,6 +3,9 @@ class Convoversation {
   constructor (obj = {}) {
     this.followUpQuestionEnum = -1
     this.userAction = 2
+    this.sendSMS = false
+    this.phoneNumber = -1
+
     obj && Object.assign(this, obj)
   }
 
@@ -13,6 +16,14 @@ class Convoversation {
 
   withUserAction (userAction) {
     this.userAction = userAction
+    return this
+  }
+  withManuallyProvidedPhoneNumber (userAction) {
+    this.userAction = userAction
+    return this
+  }
+  sendSMS (sendSMS) {
+    this.sendSMS = sendSMS
     return this
   }
 }
