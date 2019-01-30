0155bb2eff5662f344bb3926a7dbc171bfa74d8a
Ainsley Bernard <ains_bernard@hotmail.com>
2019-01-30 16:23:02 -0500

    Refactored
    Removed next/skip logic into own handler. DRY.

--- lambda/custom/handlers/builtInIntents/NextHandler.js
+++ lambda/custom/handlers/builtInIntents/NextHandler.js
@@ -2,9 +2,6 @@
 
 // Handlers
 const SearchForVehicleRecallIntentHandler = require('../customIntents/SearchForVehicleRecallHandler')
-// Enums
-const USER_ACTION = require('../../Constants').userAction
-const SESSION_KEYS = require('../../Constants').sessionKeys
 
 const NextIntentHandler = {
   canHandle (handlerInput) {
@@ -12,14 +9,8 @@ const NextIntentHandler = {
         handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent'
   },
   handle (handlerInput) {
-    const { attributesManager } = handlerInput
-    const sessionAttributes = attributesManager.getSessionAttributes()
-
-    sessionAttributes[SESSION_KEYS.CurrentRecallIndex]++
-    // set for testing purposes only when unit testing YML configuration
-    sessionAttributes[SESSION_KEYS.UserActionPerformed] = USER_ACTION.USER_ACTION.USER_ACTION.InitiatedSkip
-
-    return SearchForVehicleRecallIntentHandler.CompletedSearchForVehicleRecallIntentHandler.handle(handlerInput, USER_ACTION.InitiatedSkip)
+    // TODO: add defensive coding to when skip is said out of context
+    return SearchForVehicleRecallIntentHandler.MoveToNextRecallHandler.handle(handlerInput)
   }
 }
 
