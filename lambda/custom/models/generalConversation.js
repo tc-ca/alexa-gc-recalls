fc1ba899e5809310b3446bfd148cbde68919dd59
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-07 13:38:32 -0500

    Create generalConversation.js

---
+++ lambda/custom/models/generalConversation.js
@@ -0,0 +1,37 @@
+/**
+ * An object to hold general related conversations outside the context of specific recall lookup 
+ * 
+ * @class Conversation
+ */
+class GeneralConversation {
+  constructor (obj = {}) {
+    this.followUpQuestionCode = -1
+    this.phoneNumber = 2
+    this.sendSMS = false
+    this.phoneNumber = -1
+    this.userAction = -1
+
+    obj && Object.assign(this, obj)
+  }
+
+  
+  withFollowUpQuestion (followUpQuestionCode) {
+    this.followUpQuestionCode = followUpQuestionCode
+    return this
+  }
+
+  withUserAction (userAction) {
+    this.userAction = userAction
+    return this
+  }
+  withUserProvidedPhoneNumber (phoneNumber) {
+    this.phoneNumber = phoneNumber
+    return this
+  }
+  sendSMS (sendSMS) {
+    this.sendSMS = sendSMS
+    return this
+  }
+}
+
+module.exports = GeneralConversation
