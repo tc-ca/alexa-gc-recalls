eaf31e482e54a1fe245cc7f1bcaaa52853c78c77
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-07 13:39:13 -0500

    refactored and added comments

--- lambda/custom/services/conversationService.js
+++ lambda/custom/services/conversationService.js
@@ -1,13 +1,20 @@
 const implementjs = require('implement-js')
 const implement = implementjs.default
 const INTERFACES = require('../interfaces.js')
-const Conversation = require('../models/conversation')
 
-function GetConversationObj () {
+const MODELS = { GeneralConversation: require('../models/generalConversation') }
+
+/**
+ * functions checks to see if GeneralConversation object implements specified properties. 
+ *
+ * @returns returns specific GeneralConversation object
+ */
+function GetGeneralConversationObj () {
   // checks to see if properties are implemented
-  const obj = implement(INTERFACES.IConversation)(new Conversation())
+  implement(INTERFACES.IConversation)(new MODELS.GeneralConversation())
+  const obj = implement(INTERFACES.IGeneralConversation)(new MODELS.GeneralConversation())
   // to get intelli sense cast as Conversation object type.
-  return new Conversation(obj)
+  return new MODELS.GeneralConversation(obj)
 }
 
-module.exports = { GetConversationObj: GetConversationObj }
+module.exports = { GetGeneralConversationObj }
