13c34394fce0730326f9e4565bbd9969a9581898
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-07 13:38:24 -0500

    Refactored and added comments

--- lambda/custom/models/phoneNumber.js
+++ lambda/custom/models/phoneNumber.js
@@ -1,13 +1,19 @@
 
+/**
+ * Data transfer object which contains information on retrieval of Alexa account phone number from Alexa API
+ *
+ * @class PhoneNumber
+ */
 class PhoneNumber {
   constructor (obj = {}) {
-    this.phoneNumberState = -1
-    this.phoneNumber = null
+    // the result of Alexa API get request for end user phone number. 
+    this.apiRetrievalResult = -1
+    // country code of phone number
     this.countryCode = -1
 
+    this.phoneNumber = null
     obj && Object.assign(this, obj)
   }
-
 }
 
 module.exports = PhoneNumber
