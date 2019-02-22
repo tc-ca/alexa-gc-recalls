8497291f2a137fb65d723c4db1667b7d5ae97038
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-22 16:14:34 -0500

    Update vehicleConversation.js

--- lambda/custom/models/vehicleConversation.js
+++ lambda/custom/models/vehicleConversation.js
@@ -1,10 +1,14 @@
-class VehicleRecallSpeakText {
+const SEARCH_FINDINGS = require('../constants').VehicleSearchFindings
+const CONVERSATION_CONTEXT = require('../constants').VehicleConversationContext
+const FOLLOW_UP_QUESTIONS = require('../constants').FollowUpQuestions
+
+module.exports = class VehicleConversation {
   constructor () {
     this.result = ''
     this.overview = ''
     this.description = ''
     this.followUpQuestion = ''
-    this.followUpQuestionEnum = null
+    this.followUpQuestionCode = null
     this.hasfollowUpQuestion = false
 
     this.year = ''
@@ -16,115 +20,205 @@ class VehicleRecallSpeakText {
 
   static get Builder () {
     class Builder {
-      constructor ({ year, make, model, recalls, currentIndex }) {
+      constructor ({ year, make, model, recalls, recallsSummaries, currentIndex, requestAttributes }) {
         this.year = year
         this.make = make
         this.model = model
+        this.recallsSummaries = recallsSummaries
         this.recalls = recalls
         this.currentIndex = currentIndex
         this.followUpQuestion = ''
-        this.followUpQuestionEnum = null
-        this.hasResultText = false
+        this.followUpQuestionCode = null
+        this.hasfindings = false
         this.hasOverviewText = false
-        this.hasdescriptionText = false
-        this.hasfollowUpQuestionText = false
+        this.hasdetails = false
+        this.hasfollowUpQuestion = false
+        this.requestAttributes = requestAttributes
+        this.userAction = false
+        this.searchFindings = SEARCH_FINDINGS.SearchNotConducted
+        this.conversation = null
+        this.validVehicleModel = false
       }
-      withResult (result) {
-        console.log('prop', this.recalls.length)
-        let plural = (this.recalls.length > 1 ? 's' : '')
-        // let phonemePlural = (this.recalls.length > 1 ? 'z' : '')
-        let phonemeRecall = `recall${plural}`
 
-        if (this.recalls.length === 0) {
-          this.result = `I didn't find any recalls for your ${this.year} ${this.make} ${this.model}. `
-        } else if (this.recalls.length === 1) {
-          this.result = `<p>I've found ${this.recalls.length} ${phonemeRecall} potentially affecting your ${this.year} ${this.make} ${this.model}. </p><break time ="1s"/>`
-        } else if (this.recalls.length > 1) {
-          this.result = `<p>I've found ${this.recalls.length} ${phonemeRecall} potentially affecting your ${this.year} ${this.make} ${this.model}. 
-              I'll start with the most recent. At any time you skip to the next recall. </p><break time ="3"/> `
-        }
-
-        this.hasResultText = true
+      withConversation (conversation) {
+        this.conversation = conversation
         return this
       }
-      withOverview (overview) {
-        if (this.recalls.length > 0) {
-          this.overview = `<p>On ${this.recalls[this.currentIndex].recallDate} there was a recall affecting the ${this.recalls[this.currentIndex].componentType} of ${this.recalls[this.currentIndex].unitsAffected} vehicles</p>`
+
+      withFindings () {
+        if (this.validVehicleModel) {
+          if (this.recalls.length === 0) {
+            this.searchFindings = SEARCH_FINDINGS.NoRecallsFound
+            this.findings = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_RECALLS_FOUND_NONE`)
+          } else {
+            if (hasSimilarModelsAffectedByRecall(this.recalls, this.model)) {
+              this.searchFindings = SEARCH_FINDINGS.AmbigiousModelFound
+              this.findings = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL`)
+                .replace('%VehicleRecallYear%', this.year)
+                .replace('%VehicleRecallMake%', this.make)
+                .replace('%VehicleRecallModel%', this.model)
+            } else {
+              if (this.recalls.length === 1) {
+                this.searchFindings = SEARCH_FINDINGS.SingleRecallFound
+                this.findings = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_RECALLS_FOUND_ONE`)
+                  .replace('%VehicleRecallYear%', this.year)
+                  .replace('%VehicleRecallMake%', this.make)
+                  .replace('%VehicleRecallModel%', this.model)
+              } else if (this.recalls.length > 1) {
+                this.searchFindings = SEARCH_FINDINGS.MultipleRecallsFound
+                this.findings = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE`)
+                  .replace('%RecallCount%', this.recalls.length)
+                  .replace('%VehicleRecallYear%', this.year)
+                  .replace('%VehicleRecallMake%', this.make)
+                  .replace('%VehicleRecallModel%', this.model)
+              }
+            }
+          }
+        } else {
+          this.searchFindings = SEARCH_FINDINGS.NonValidModelFound
+          this.findings = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_SLOT_VALUES_NOT_FOUND`)
+            .replace('%VehicleRecallYear%', this.year)
+            .replace('%VehicleRecallMake%', this.make)
+            .replace('%VehicleRecallModel%', this.model)
+        }
+        console.log('Vehicle Recall Search Result =', this.findings)
+        this.hasfindings = true
+        return this
+      }
+
+      withIntro (includeIntro) {
+        if (includeIntro) {
+          this.overview = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_RECALL_READING_INTRO`)
           this.hasOverviewText = true
         }
         return this
       }
-      withDescription (description) {
+      modelIsValid (isValidModel) {
+        if (isValidModel) {
+          this.validVehicleModel = isValidModel
+        }
+        return this
+      }
+      withDetails () {
         if (this.recalls.length > 0) {
-          this.description = `<p>${this.recalls[this.currentIndex].description} </p>`
-          this.hasdescriptionText = true
+          this.details = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_RECALL_READING_DETAILS`)
+            .replace('%VehicleRecallDate%', this.recallsSummaries[this.currentIndex].recallDate)
+            .replace('%VehicleRecallComponent%', this.recallsSummaries[this.currentIndex].componentType)
+            .replace('%VehicleRecallDetails%', this.recallsSummaries[this.currentIndex].description)
+
+          this.hasdetails = true
         }
         return this
       }
       withFollowUpQuestion (convoContext) {
         switch (convoContext) {
-          case CONVO_CONTEXT.RecallInfo:
+          case CONVERSATION_CONTEXT.ReadingRecallState:
             if (this.recalls.length !== 0) {
               if (this.currentIndex === this.recalls.length - 1) {
-                this.followUpQuestion = 'Would you like to search for another recall? '
-                this.followUpQuestionEnum = SearchVehicleRecallIntentYesNoQuestions.WouldYouLikeToSearchForAnotherRecall
+                this.followUpQuestion = this.requestAttributes
+                  .t(`SPEECH_TXT_VEHICLE_RECALL_READING_DONE`)
+                  .replace('%VehicleRecallYear%', this.year)
+                  .replace('%VehicleRecallMake%', this.make)
+                  .replace('%VehicleRecallModel%', this.model)
+                this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.WouldYouLikeTheRecallInformationRepeated
               } else {
-                this.followUpQuestion = 'Would you like to hear the next recall? '
-                this.followUpQuestionEnum = SearchVehicleRecallIntentYesNoQuestions.WouldYouLikeToHearTheNextRecall
+                this.followUpQuestion = this.requestAttributes.t(`SPEECH_TXT_VEHCILE_RECALLS_READING_FOLLOW_UP_QUESTION_WOULD_YOU_LIKE_TO_HEAR_NEXT_RECALL`)
+                this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.WouldYouLikeToHearTheNextRecall
               }
             } else {
-              this.followUpQuestion = 'Would you like to search for another recall? '
-              this.followUpQuestionEnum = SearchVehicleRecallIntentYesNoQuestions.WouldYouLikeToSearchForAnotherRecall
+              this.followUpQuestion = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE`)
+              this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.WouldYouLikeToSearchForAnotherRecall
             }
             break
-          case CONVO_CONTEXT.AmbiguousModel:
-            this.followUpQuestion = `I've found a few different models of 
-            ${this.year} ${this.make} ${this.model} impacted by a recall, is yours a ${BuildSimilarModelsString(this.recalls)}?`
-            this.followUpQuestionEnum = SearchVehicleRecallIntentYesNoQuestions.ProvideComfirmationOfAmbiguousModel
+          case CONVERSATION_CONTEXT.GettingSearchResultFindingsState:
+            if (this.validVehicleModel) {
+              if (this.recalls.length === 0) {
+                this.followUpQuestion = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE`)
+                this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.WouldYouLikeToSearchForAnotherRecall
+              } else {
+                if (hasSimilarModelsAffectedByRecall(this.recalls, this.model)) {
+                  this.followUpQuestion = this.requestAttributes
+                    .t(`VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL`)
+                    .replace('%AmbigiousModelsList%', BuildSimilarModelsString(this.recalls, this.model))
+                  this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.VEHICLE_IsItModelAOrModelB
+                } else {
+                  if (this.recalls.length === 1) {
+                    this.followUpQuestion = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE`)
+                    this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.WouldYouLikeToMeReadTheRecall
+                  } else if (this.recalls.length > 1) {
+                    this.followUpQuestion = this.requestAttributes.t(`SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE`)
+                    this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.WouldYouLikeToMeReadTheRecall
+                  }
+                }
+              }
+            } else {
+              this.followUpQuestion = this.requestAttributes.t('SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NON_VALID')
+              this.followUpQuestionCode = FOLLOW_UP_QUESTIONS.VEHICLE_MODEL_NON_VALID_WhatIsYourVehicleModel
+            }
             break
           default:
             break
         }
 
-        this.hasfollowUpQuestionText = true
+        this.hasfollowUpQuestion = true
+        return this
+      }
 
+      withUserAction (userAction) {
+        this.userAction = userAction
         return this
       }
 
       speech () {
-        console.log('speech', this.result + this.overview + this.description + this.followUpQuestion)
-
         let msg = ''
-        if (this.hasResultText) {
-          msg += this.result
+        if (this.hasfindings) {
+          msg += this.findings + ' '
         }
         if (this.hasOverviewText) {
-          msg += this.overview
+          msg += this.overview + ' '
         }
-        if (this.hasdescriptionText) {
-          msg += this.description
+        if (this.hasdetails) {
+          msg += this.details + ' '
         }
-        if (this.hasfollowUpQuestionText) {
-          msg += this.followUpQuestion
+        if (this.hasfollowUpQuestion) {
+          msg += this.followUpQuestion + ' '
         }
-
         return msg
       }
       build () {
-        return new VehicleRecallSpeakText(this)
+        return new VehicleConversation(this)
       }
     }
     return Builder
   }
 }
 
-function BuildSimilarModelsString (recalls) {
-  if (Array.isArray(recalls)) {
-    let uniqueModels = [...new Set(recalls.map(x => x.modelName))]
-
-    console.log(`${uniqueModels.slice(0, -1).join(', ')}${(uniqueModels.length > 1 ? ' or ' : '')}${uniqueModels.slice(-1)[0]}`)
-    return `${uniqueModels.slice(0, -1).join(', ')}${(uniqueModels.length > 1 ? ' or ' : '')}${uniqueModels.slice(-1)[0]}`
+function hasSimilarModelsAffectedByRecall (recalls, targetedModelName) {
+  let similarModelCount = 0
+  for (let index = 0; index < recalls.length; index++) {
+    if (recalls[index].modelName !== targetedModelName && recalls[index].modelName.includes(targetedModelName)) {
+      similarModelCount++
+    }
   }
+
+  if (similarModelCount >= 1) {
+    return true
+  }
+  return false
 }
 
-module.exports = VehicleRecallSpeakText
+function BuildSimilarModelsString (recalls, targetedModelName) {
+  if (Array.isArray(recalls)) {
+    let similarModels = []
+
+    for (let index = 0; index < recalls.length; index++) {
+      if (recalls[index].modelName.includes(targetedModelName)) {
+        similarModels.push(recalls[index].modelName)
+      }
+    }
+
+    let uniqueModels = [...new Set(similarModels)]
+
+    return `${uniqueModels.slice(0, -1).join(', <break time="200ms"/> ')}${(uniqueModels.length > 1 ? ' <break time="200ms"/> or  ' : '')}${uniqueModels.slice(-1)[0]}`
+  }
+}
