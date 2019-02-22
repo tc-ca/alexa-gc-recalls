cc932f29da2768d81b5b42c14e6a1c9482e7a7ec
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-22 16:07:58 -0500

    Update NoHandler.js

--- lambda/custom/handlers/builtInIntents/NoHandler.js
+++ lambda/custom/handlers/builtInIntents/NoHandler.js
@@ -1,16 +1,13 @@
 'use strict'
-const Conversation = require('../../models/conversation')
 
 const HANDLERS = {
-  VehicleRecallHandler: require('../customIntents/SearchForVehicleRecallHandler'),
-  PhoneNumberHandler: require('../customIntents/PhoneNumberHandler'),
-  RestartSearchForRecallHandler: require('./StartOverHandler'),
-  CancelAndStopHandler: require('./CancelAndStopHandler')
+  VehicleRecallHandler: require('../customIntents/searchForVehicleRecallHandler'),
+  RestartSearchForRecallHandler: require('./startOverHandler'),
+  CancelAndStopHandler: require('./cancelAndStopHandler')
 }
 // Enums
-const SESSION_KEYS = require('../../Constants').sessionKeys
-const QUESTION = require('../../Constants').SearchVehicleRecallIntentYesNoQuestions
-const USER_ACTION = require('../../Constants').userAction
+const SESSION_KEYS = require('../../constants').sessionKeys
+const QUESTION = require('../../constants').FollowUpQuestions
 
 const NoIntentHandler = {
   canHandle (handlerInput) {
@@ -21,30 +18,12 @@ const NoIntentHandler = {
     const { attributesManager } = handlerInput
     const sessionAttributes = attributesManager.getSessionAttributes()
 
-    const convo = new Conversation(sessionAttributes[SESSION_KEYS.Conversation])
     const vehicleConversation = sessionAttributes[SESSION_KEYS.VehicleConversation]
 
-    switch (sessionAttributes[SESSION_KEYS.LogicRoutedIntentName]) {
-      case 'SelectRecallCategoryIntent':
-        switch (convo.followUpQuestionEnum) {
-          case QUESTION.WouldYouLikeToRecieveSMSMessage:
-
-            convo.withUserAction = USER_ACTION.ResponsedNoToWantingToReceiveSMS
-            sessionAttributes[SESSION_KEYS.Conversation] = convo
-
-            return HANDLERS.PhoneNumberHandler.SMSHandler.handle(handlerInput, USER_ACTION.ResponsedNoToWantingToReceiveSMS)
-
-          case QUESTION.WouldYouLikeToSearchForAnotherRecall:
-
-            return HANDLERS.CancelAndStopHandler.handle(handlerInput)
-
-          default:
-            break
-        }
-        break
+    switch (sessionAttributes[SESSION_KEYS.CurrentIntentLocation]) {
       case 'SearchForVehicleRecallIntent':
 
-        switch (vehicleConversation.followUpQuestionEnum) {
+        switch (vehicleConversation.followUpQuestionCode) {
           case QUESTION.WouldYouLikeToMeReadTheRecall:
             return HANDLERS.VehicleRecallHandler.SearchForAnotherRecallHandler.handle(handlerInput)
           case QUESTION.WouldYouLikeToSearchForAnotherRecall:
@@ -57,7 +36,7 @@ const NoIntentHandler = {
         break
       case 'ReadVehicleRecallHandler':
 
-        switch (vehicleConversation.followUpQuestionEnum) {
+        switch (vehicleConversation.followUpQuestionCode) {
           case QUESTION.WouldYouLikeToHearTheNextRecall:
 
             return HANDLERS.VehicleRecallHandler.SearchForAnotherRecallHandler.handle(handlerInput)
@@ -73,15 +52,8 @@ const NoIntentHandler = {
         break
       case 'DeniedCompletedSearchForVehicleRecallIntentHandler':
         return HANDLERS.CancelAndStopHandler.handle(handlerInput)
-      case 'SMSIntent':
-        switch (convo.followUpQuestionEnum) {
-          case QUESTION.IsYourPhoneNumberFiveFiveFiveBlahBlah:
-
-            return HANDLERS.PhoneNumberHandler.SMSHandler.handle(handlerInput, USER_ACTION.ResponsedNoToCorrectPhoneNumberFoundOnAccount)
-        }
-        break
       case 'GetSearchForAnotherRecallQuestionHandler':
-        switch (vehicleConversation.followUpQuestionEnum) {
+        switch (vehicleConversation.followUpQuestionCode) {
           case QUESTION.WouldYouLikeToSearchForAnotherRecall:
             return HANDLERS.CancelAndStopHandler.handle(handlerInput)
         }
