f715712848b721566a00bb1a1d4c521afeed8620
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-22 16:12:47 -0500

    update language strings

--- lambda/custom/i18n/en-US.js
+++ lambda/custom/i18n/en-US.js
@@ -1,59 +1,53 @@
 
-const SKILL_NAME = 'Canadian Safety Recalls'
+// CONSTANTS
+const TELL_ME_YOUR_MAKE = `Let's get started. What is the make of your vehicle?`
+const VEHICLE_MAKE_MODEL_YEAR = `%%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%%`
+const RECALL_COUNT = `%%RecallCount%%`
+const RECALL_DATE = `%%VehicleRecallDate%%`
+const RECALL_COMPONENT = `%%VehicleRecallComponent%%`
+const RECALL_DETAILS = `%%VehicleRecallDetails%%`
+const AMBIGIOUS_MODELS = `%%AmbigiousModelsList%%`
+const TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER = `<say-as interpret-as="telephone">18003330510</say-as>`
+
+// ALEXA'S PRONUNCIATION
+const PHONEMES = {
+  recall: `<phoneme alphabet="ipa" ph="ri.kɔ:l">recall</phoneme>`,
+  recalls: `<phoneme alphabet="ipa" ph="ri.kɔ:lz">recalls</phoneme>` }
 
 module.exports = {
   translation: {
-    'SKILL_NAME': 'Super Welcome', // <- can either be a string...
-    'GREETING': [ // <- or an array of strings.
-      'Hello there',
-      'Hey',
-      'Hi!'
-    ],
-    'GREETING_WITH_NAME': [
-      'Hey %s', // --> That %s is a wildcard. It will
-      'Hi there, %s', //     get turned into a name in our code.
-      'Hello, %s' //     e.g. requestAttributes.t('GREETING_WITH_NAME', 'Andrea')
-    ],
-    WELCOME_LONG:
-    `Hi I'm your ` +
-    SKILL_NAME +
-    `, Assistant. I help Canadians like you find information on different kinds of recalls.
-      <break time ="1s"/>
-      If you would like information on vehicle recalls, say vehicle.
-    `,
-    WOULD_YOU_LIKE_TO_RECIEVE_TXT_MSG_ON_PHONE: `Great! I can help you find %%RecallCategory%% recall info by reading it out loud, texting it to you, or both.
-                        <break time ="1s"/>        
-                        Would you like to get the recall information by text on your phone?
-                        `,
-    IS_YOUR_PHONE_NUMBER_1_XXX_XXX_XXXX: `Alright, I can send any vehicle recall info we find to the phone number associated with your Alexa Account. Is your phone number <say-as interpret-as="telephone">%%PhoneNumber%%</say-as>?`,
-    TELL_ME_YOUR_MAKE: `Okay. Could you please tell me the make of your vehicle for example, Honda?`,
-    TELL_ME_YOUR_PHONE_NUMBER: `Alright, what is your phone number?`,
-    ERROR_PHONE_NUMBER_VALIDATION_10_DIGIT_TRY_AGAIN: `Sorry, the number provided must be ten digits, please say again`,
-    VEHICLE_RECALLS_FOUND_NONE: `There are no recalls associated with your vehicle in our system. `,
-    VEHICLE_RECALLS_FOUND_ONE: `<p>I've found a <phoneme alphabet="ipa" ph="ri.kɔ:l">recall</phoneme> potentially affecting your %%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%%. %%SentSMSMsg%%</p>`,
-    VEHICLE_RECALLS_FOUND_MULTIPLE: `<p>I've found %%RecallCount%% <phoneme alphabet="ipa" ph="ri.kɔ:lz">recalls</phoneme> potentially affecting your %%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%%. %%SentSMSMsg%%</p>`,
-    VEHICLE_RECALLS_FOUND_NON_VALID: `I'm sorry. I don't have any information on your %%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%% at the moment. I'm still a voice assistant in training. Please contact my human friends at Transport Canada Recalls customer suppport at <say-as interpret-as="telephone">18003330510 for more help.</say-as>`,
-    VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `I've found a few different models of %%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%%.`,
-    VEHICLE_RECALLS_FOUND_READ_COMFIRMATION: `Would you like me to read it to you?`,
-    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE: `Would you like to search for another vehicle recall?`,
-    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE: `Would you like me to read it to you?`,
-    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE: `Would you like me to read it to you?`,
-    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NON_VALID: `Would you like to search for another recall?`,
-    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Is yours a %%AmbigiousModelsList%%?`,
-    VEHICLE_IN_PROGRESS_SEARCHING_MSG_1: `Ok, let me look for your vehicle.<break time="500ms"/>`,
-    VEHICLE_IN_PROGRESS_SEARCHING_MSG_2: `Alright, let me check that our for you.<break time="500ms"/>`,
-    VEHICLE_RECALL_READING_INTRO_USAGE: `Alright, if you find that I'm rambling too much, just say skip, and I'll  skip to the next recall. At any point you can leave the skill by saying exit<break time="500ms"/>`,
-    VEHICLE_DETAILS_MSG: `<p>On %%VehicleRecallDate%% there was a recall affecting the %%VehicleRecallComponent%%.<break time="500ms"/>%%VehicleRecallDetails%%.</p>`,
-    VEHICLE_RECALLS_END_MSG: `<p>That's all the information I have for your %%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%% vehicle. Do you want me to repeat the recall information for this vehicle?  </p>`,
-    VEHICLE_RECALLS_SENT_SMS: `I've sent you a text message with your vehicle recall information.`,
-    VEHICLE_RECALLS_TRY_SEARCH_AGAIN: `Would you like to try to search again?`,
-    VEHCILE_RECALLS_GOODBYE_MSG: `OK! Have a good day!`,
-    VEHCILE_RECALLS_LOOK_FOR_ANOTHER_RECALL: `Do you want me to look for recalls for another recall?`,
-    VEHCILE_RECALLS_FOLLOW_UP_QUESTION_LISTEN_TO_NEXT_RECALL_MSG: `Would you like to hear the next recall?`,
-    ERROR_MISSING_PHONE_NUMBER_PERMISSIONS: 'Please enable phone number permissions in the Amazon Alexa app.',
-    ERROR_MISSING_PHONE_NUMBER: 'You can set your phone number at Amazon.com, under log-in and security.',
-    GENERAL_ERROR_MSG: 'Uh Oh. Looks like something went wrong.'
+    // HELLO STRING
+    SPEECH_TXT_VEHICLE_WELCOME_MSG: `Hello! I'm your Canadian Safety Recalls Assistant. I can help you find vehicle ${PHONEMES.recall} information. ${TELL_ME_YOUR_MAKE}`,
 
-    // ...more...
+    // GOODBYE STRING
+    SPEECH_TXT_VEHCILE_RECALLS_GOODBYE: `OK! Have a good day!`,
+
+    // SEARCH RESULT STRINGS
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_NONE: `There are no ${PHONEMES.recalls} associated with your vehicle in our system. `,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_ONE: `<p>I've found a ${PHONEMES.recall} potentially affecting your ${VEHICLE_MAKE_MODEL_YEAR}. I've sent you a text message with your vehicle ${PHONEMES.recall} information.</p>`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `<p>I've found ${RECALL_COUNT} ${PHONEMES.recalls} potentially affecting your ${VEHICLE_MAKE_MODEL_YEAR}. I've sent you a text message with your vehicle ${PHONEMES.recall} information.</p>`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_NON_VALID: `I'm sorry. I don't have any information on your ${VEHICLE_MAKE_MODEL_YEAR} at the moment. I'm still a voice assistant in training. Please contact my human friends at Transport Canada Recalls customer suppport at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help.`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `Hmm... I've found a few different models of ${VEHICLE_MAKE_MODEL_YEAR}.`,
+    SPEECH_TXT_VEHICLE_SLOT_VALUES_NOT_FOUND: `Hmm...I didn't find your ${VEHICLE_MAKE_MODEL_YEAR} in my database. To help me with my search please only include the vehicle model name and not any trim packages.`,
+
+    // SEARCH RESULT FOLLOW UP QUESTION STRINGS
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE: `Would you like to search for another vehicle ${PHONEMES.recall}?`,
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE: `Would you like me to read it to you?`,
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE: `Would you like me to read it to you?`,
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NON_VALID: `What is your vehicle model?`,
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Is yours a ${AMBIGIOUS_MODELS}?`,
+    // READING RECALLS STRINGS
+    SPEECH_TXT_VEHICLE_RECALL_READING_INTRO: `While I'm reading you the ${PHONEMES.recall} information, at any point you can skip to the next ${PHONEMES.recall} by saying "Alexa skip". `,
+    SPEECH_TXT_VEHICLE_RECALL_READING_DETAILS: `<p>On ${RECALL_DATE} there was a ${PHONEMES.recall} affecting the ${RECALL_COMPONENT}.<break time="500ms"/> ${RECALL_DETAILS}</p>`,
+    SPEECH_TXT_VEHICLE_RECALL_READING_DONE: `<p>That's all the information I have for your ${VEHICLE_MAKE_MODEL_YEAR} vehicle. Do you want me to repeat the ${PHONEMES.recall} information for this vehicle?  </p>`,
+    SPEECH_TXT_VEHCILE_RECALLS_READING_FOLLOW_UP_QUESTION_WOULD_YOU_LIKE_TO_HEAR_NEXT_RECALL: `Would you like to hear the next ${PHONEMES.recall}?`,
+
+    // MISCELLANEOUS STRINGS
+    SPEECH_TXT_VEHICLE_TELL_ME_YOUR_MAKE: `${TELL_ME_YOUR_MAKE}`,
+    SPEECH_TXT_VEHICLE_WOULD_YOU_LIKE_TO_SEARCH_AGAIN: `Would you like to try to search again?`,
+    SPEECH_TXT_VEHCILE_SEARCH_FOR_ANOTHER_RECALL: `Do you want me to look for ${PHONEMES.recalls} for another vehicle?`,
+
+    // ERROR STRINGS
+    SPEECH_TXT_VEHICLE_MODEL_VALIDATION_FAILED: `I'm sorry, I don't have any information on your ${VEHICLE_MAKE_MODEL_YEAR} at the moment. I'm still a voice assistant in training. Please contact my human friends at Transport Canada Recalls customer support at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help.`
   }
 }
--- lambda/custom/i18n/fr-CA.js
+++ lambda/custom/i18n/fr-CA.js
@@ -1,22 +1,41 @@
-
-const SKILL_NAME = 'Canadian Safety Recalls'
+const TELL_ME_YOUR_MAKE = `On peut commencer. Quelle est la marque de ton véhicule?`
+const VEHICLE_MAKE_MODEL_YEAR = `%%VehicleRecallYear%% %%VehicleRecallMake%% %%VehicleRecallModel%%`
+const RECALL_COUNT = `%%RecallCount%%`
+const RECALL_DATE = `%%VehicleRecallDate%%`
+const RECALL_COMPONENT = `%%VehicleRecallComponent%%`
+const RECALL_DETAILS = `%%VehicleRecallDetails%%`
+const AMBIGIOUS_MODELS = `%%AmbigiousModelsList%%`
+// const PHONEMES = { }
+const HELP_LINE_PHONE_NUMBER = `<say-as interpret-as="telephone">18003330510</say-as>`
 
 module.exports = {
   translation: {
-    'SKILL_NAME': 'Super Welcome', // <- can either be a string...
-    'GREETING': [ // <- or an array of strings.
-      'Hello there',
-      'Hey',
-      'Hi!'
-    ],
-    'GREETING_WITH_NAME': [
-      'Hey %s', // --> That %s is a wildcard. It will
-      'Hi there, %s', //     get turned into a name in our code.
-      'Hello, %s' //     e.g. requestAttributes.t('GREETING_WITH_NAME', 'Andrea')
-    ],
-    WELCOME_LONG:
-    `Translation required`,
-    RECIEVE_SMS_QUESTION: `Translation required`
+    WELCOME_MSG: `Bonjour! Je suis votre assistante pour les rappels de sécurité. Je peux t'informer sur les rappels de sécurité au Canada. ${TELL_ME_YOUR_MAKE }`,
+    VEHICLE_SLOT_VALUES_NOT_FOUND: `Hmm...Je n’ai pas trouvé ton ${VEHICLE_MAKE_MODEL_YEAR} dans notre base de données. Pour m’aider à chercher, je te demanderais de seulement inclure le nom du modèle de ton véhicule sans le niveau d’équipement. Quel est le nom du modèle de ton véhicule?`,
+    TELL_ME_YOUR_MAKE: `${TELL_ME_YOUR_MAKE}`,
+    VEHICLE_RECALLS_FOUND_NONE: `Je n’ai pas trouvé de rappels associés avec ton véhicule dans notre système.`,
+    VEHICLE_RECALLS_FOUND_ONE: `<p>J’ai trouvé un rappel qui pourrait affecter ton ${VEHICLE_MAKE_MODEL_YEAR}. %%SentSMSMsg%%</p>`,
+    VEHICLE_RECALLS_FOUND_MULTIPLE: `<p>J’ai trouvé ${RECALL_COUNT} rappels qui pourraient affecter ton ${VEHICLE_MAKE_MODEL_YEAR}. %%SentSMSMsg%%</p>`,
+    VEHICLE_RECALLS_FOUND_NON_VALID: `Je m'excuse. Je n’ai pas d’information sur ton ${VEHICLE_MAKE_MODEL_YEAR} en ce moment. Je suis encore un assistant vocal en formation. Si tu veux de l’aide, tu peux contacter mes collègues humains aux services à la clientèle Rappels Transports Canada en composant le ${HELP_LINE_PHONE_NUMBER}.`,
+    VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `Hmm...J’ai besoin de plus d’information sur le modèle.`,
+    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE: `Veux-tu que je trouve les rappels de sécurité d’un autre véhicule?`, 
+    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE: `Veux-tu que je lise les rappels?`,
+    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE: `Veux-tu que je lise les rappels?`,
+    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NON_VALID: `What is your vehicle model?`,
+    VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Est-ce que le tien est un ${AMBIGIOUS_MODELS}?`,
+    VEHICLE_RECALL_READING_INTRO_USAGE: `Lorsque je vais lire l’information sur les rappels de véhicules, tu pourras sauter au prochain rappel à n’importe quel moment en disant Alexa, prochain.`,
+    VEHICLE_DETAILS_MSG: `<p>Le ${RECALL_DATE}, il y avait un rappel de sécurité affectant le ${RECALL_COMPONENT}.<break time="500ms"/>${RECALL_DETAILS}.</p>`,
+    VEHICLE_RECALLS_END_MSG: `<p>C’est tout ce que j’ai comme information sur ton ${VEHICLE_MAKE_MODEL_YEAR}. Veux-tu que je répète l’information sur les rappels de sécurité pour ce véhicule?</p>`,
+    VEHICLE_RECALLS_SENT_SMS: `Je t'ai envoyé l’information sur les rappels par texto/courriel`,
+    VEHICLE_RECALLS_TRY_SEARCH_AGAIN: `Veux-tu essayer de chercher encore une fois?`,
+    VEHCILE_RECALLS_GOODBYE_MSG: `OK! Passe une bonne journée!`,
+    VEHCILE_RECALLS_LOOK_FOR_ANOTHER_RECALL: `Veux-tu que je trouve les rappels de sécurité d’un autre véhicule?`,
+    VEHCILE_RECALLS_FOLLOW_UP_QUESTION_LISTEN_TO_NEXT_RECALL_MSG: `Veux-tu entendre le prochain rappel?`,
+    ERROR_MISSING_PHONE_NUMBER_PERMISSIONS: 'Please enable phone number permissions in the Amazon Alexa app.',
+    ERROR_MISSING_PHONE_NUMBER: 'You can set your phone number at Amazon.com, under log-in and security.',
+    GENERAL_ERROR_MSG: 'Uh Oh. Looks like something went wrong.',
+    VEHICLE_MODEL_VALIDATION_FAILED: `Je m'excuse. Je n’ai pas d’information sur ton ${VEHICLE_MAKE_MODEL_YEAR} en ce moment. Je suis encore un assistant vocal en formation. Si tu veux de l’aide, tu peux contacter mes collègues humains aux services à la clientèle Rappels Transports Canada en composant le ${HELP_LINE_PHONE_NUMBER}.`,
+    COMMAND_OUT_OF_CONTEXT: `Hmm... I was not expecting that response, do you need help?`
     // ...more...
   }
 }
