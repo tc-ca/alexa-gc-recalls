8bf97a63e8f95338f3b38daf6aa215cbf2a1f7dd
Ainsley Bernard <ains_bernard@hotmail.com>
2019-02-22 16:14:27 -0500

    Update phoneNumber.js

--- lambda/custom/models/phoneNumber.js
+++ lambda/custom/models/phoneNumber.js
@@ -6,7 +6,7 @@
  */
 class PhoneNumber {
   constructor (obj = {}) {
-    // the result of Alexa API get request for end user phone number. 
+    // the result of Alexa API get request for end user phone number.
     this.apiRetrievalResult = -1
     // country code of phone number
     this.countryCode = -1
