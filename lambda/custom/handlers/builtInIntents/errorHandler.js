7baccdd01058d9b12f4087ec04fb2e9ae21e7cec
Ainsley Bernard <ains_bernard@hotmail.com>
2019-03-01 10:43:53 -0500

    :pencil: Update errorHandler.js

--- lambda/custom/handlers/builtInIntents/errorHandler.js
+++ lambda/custom/handlers/builtInIntents/errorHandler.js
@@ -3,13 +3,31 @@ const ErrorHandler = {
     return true
   },
   handle (handlerInput, error) {
+    const { attributesManager } = handlerInput
+    const requestAttributes = attributesManager.getRequestAttributes()
+    const speechText = requestAttributes.t('SPEECH_TXT_VEHICLE_ERROR_GENERIC_MESSAGE')
     console.log(`Error handled: ${error.message}`)
 
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .speak(speechText)
+      .reprompt(speechText)
       .getResponse()
   }
 }
 
-module.exports = ErrorHandler
+const CommandOutOfContextHandler = {
+  handle (handlerInput) {
+    const { attributesManager } = handlerInput
+
+    const requestAttributes = attributesManager.getRequestAttributes()
+    const speechText = requestAttributes.t('SPEECH_TXT_VEHICLE_ERROR_COMMAND_OUT_OF_CONTEXT')
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      // .withSimpleCard('Hello World', speechText)
+      .getResponse()
+  }
+}
+
+module.exports = { ErrorHandler, CommandOutOfContextHandler }
