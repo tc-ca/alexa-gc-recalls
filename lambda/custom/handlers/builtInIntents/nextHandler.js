514e8eec4401ec5a4ba75b0c9f6d4702f8b6c8c9
Ainsley Bernard <ains_bernard@hotmail.com>
2019-03-01 11:54:19 -0500

    :pencil: Update nextHandler.js
    Add handler when Next Intent is called out of context.

--- lambda/custom/handlers/builtInIntents/nextHandler.js
+++ lambda/custom/handlers/builtInIntents/nextHandler.js
@@ -1,17 +1,43 @@
 'use strict'
 
 // Handlers
-const SearchForVehicleRecallIntentHandler = require('../customIntents/searchForVehicleRecallHandler')
+const HANDLERS = {
+  VehicleRecallHandler: require('../customIntents/searchForVehicleRecallHandler'),
+  ErrorHandler: require('../builtInIntents/errorHandler')
+}
 
+/**
+ * When called, moves listener to next recall in array of recalls.
+ * @param {*} handlerInput
+ * @returns
+ */
 const NextIntentHandler = {
   canHandle (handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent'
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent' &&
+        handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation &&
+        handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation === 'ReadVehicleRecallHandler'
   },
   handle (handlerInput) {
-    // TODO: add defensive coding to when skip is said out of context
-    return SearchForVehicleRecallIntentHandler.MoveToNextRecallHandler.handle(handlerInput)
+    return HANDLERS.VehicleRecallHandler.MoveToNextRecallHandler.handle(handlerInput)
   }
 }
 
-module.exports = NextIntentHandler
+/**
+ * Executed when next intent is called out of context. i.e. user not in recall read portion of script.
+ * @param {*} handlerInput
+ * @returns
+ */
+const NextIntentHandlerOutOfContext = {
+  canHandle (handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent' &&
+        handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation &&
+        handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation !== 'ReadVehicleRecallHandler'
+  },
+  handle (handlerInput) {
+    return HANDLERS.ErrorHandler.CommandOutOfContextHandler.handle(handlerInput)
+  }
+}
+
+module.exports = { NextIntentHandler, NextIntentHandlerOutOfContext }
