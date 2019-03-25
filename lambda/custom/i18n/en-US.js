97a59e097623c96d3917af8d5093156a39d00af5
Ainsley Bernard <ains_bernard@hotmail.com>
2019-03-25 07:32:47 -0400

    :earth_americas: Update language strings

--- lambda/custom/i18n/en-US.js
+++ lambda/custom/i18n/en-US.js
@@ -1,6 +1,5 @@
 
 // CONSTANTS
-const TELL_ME_YOUR_MAKE = `Let's get started. What is the make of your vehicle?`
 const MAKE_ID = `%%VehicleRecallMakeId%%`
 const MAKE = `%%VehicleRecallMake%%`
 const MODEL = `%%VehicleRecallModel%%`
@@ -18,21 +17,26 @@ const TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER = `<say-as interpret-as="telephon
 
 // ALEXA'S PRONUNCIATION
 const PHONEMES = {
-  recall: `<phoneme alphabet="ipa" ph="ri.kɔ:l">recall</phoneme>`,
-  recalls: `<phoneme alphabet="ipa" ph="ri.kɔ:lz">recalls</phoneme>` }
+  recall: `<phoneme alphabet='ipa' ph='ri.ka:l'>recall</phoneme>`,
+  recalls: `<phoneme alphabet='ipa' ph='ri.ka:lz'>recalls</phoneme>` }
 
 module.exports = {
   translation: {
-    // HELLO STRING
-    SPEECH_TXT_VEHICLE_WELCOME_MSG: `Hello! I'm your Canadian Safety Recalls Assistant. I can help you find vehicle ${PHONEMES.recall} information. ${TELL_ME_YOUR_MAKE}`,
+    // LAUNCH STRING
+    SPEECH_TXT_VEHICLE_WELCOME_MSG: `Hello! I'm your Canadian Safety ${PHONEMES.recalls} Assistant. I can help you find vehicle ${PHONEMES.recall} information.`,
+    // LAUNCH REPROMPT
+    SPEECH_TXT_VEHICLE_MAKE_REPROMPT: `What brand makes your model?`,
 
     // GOODBYE STRING
     SPEECH_TXT_VEHCILE_RECALLS_GOODBYE: `OK! Have a good day!`,
 
+    // COMFIRMATION OF MAKE, MODEL, AND YEAR
+    SPEECH_TXT_VEHCILE_COMFIRM_MAKE_MODEL_YEAR: `Just to make sure I understood correctly, are you looking for a ${VEHICLE_MAKE_MODEL_YEAR}?`,
+
     // SEARCH RESULT STRINGS
     SPEECH_TXT_VEHICLE_RECALLS_FOUND_NONE: `There are no ${PHONEMES.recalls} associated with your vehicle in our system.`,
-    SPEECH_TXT_VEHICLE_RECALLS_FOUND_ONE: `<p>I've found a ${PHONEMES.recall} potentially affecting your ${VEHICLE_MAKE_MODEL_YEAR}. I've sent you a text message with your vehicle ${PHONEMES.recall} information.</p>`,
-    SPEECH_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `<p>I've found ${RECALL_COUNT} ${PHONEMES.recalls} potentially affecting your ${VEHICLE_MAKE_MODEL_YEAR}. I've sent you a text message with your vehicle ${PHONEMES.recall} information.</p>`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_ONE: `I've found a ${PHONEMES.recall} potentially affecting your ${VEHICLE_MAKE_MODEL_YEAR}. I've sent you a text message with your vehicle ${PHONEMES.recall} information.`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `I've found ${RECALL_COUNT} ${PHONEMES.recalls} potentially affecting your ${VEHICLE_MAKE_MODEL_YEAR}. I've sent you a text message with your vehicle ${PHONEMES.recall} information.`,
     SPEECH_TXT_VEHICLE_RECALLS_FOUND_NON_VALID: `I'm sorry. I don't have any information on your ${VEHICLE_MAKE_MODEL_YEAR} at the moment. I'm still a voice assistant in training. Please contact my human friends at Transport Canada Recalls customer suppport at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help.`,
     SPEECH_TXT_VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `Hmm... I've found a few different models of ${VEHICLE_MAKE_MODEL_YEAR}.`,
     SPEECH_TXT_VEHICLE_SLOT_VALUES_NOT_FOUND: `Hmm...I didn't find your ${VEHICLE_MAKE_MODEL_YEAR} in my database. To help me with my search please only include the vehicle model name and not any trim packages.`,
@@ -41,17 +45,16 @@ module.exports = {
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE: `Would you like to search for another vehicle ${PHONEMES.recall}?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE: `Would you like me to read it to you?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE: `Would you like me to read it to you?`,
-    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NON_VALID: `What is your vehicle model?`,
-    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Is yours a ${AMBIGIOUS_MODELS}?`,
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Is yours a ${AMBIGIOUS_MODELS} or none of these?`,
+    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND__NON_VALID: `Would you like to me run a new vehicle ${PHONEMES.recall} search?`,
 
     // READING RECALLS STRINGS
     SPEECH_TXT_VEHICLE_RECALLS_READING_INTRO: `While I'm reading you the ${PHONEMES.recall} information, at any point you can skip to the next ${PHONEMES.recall} by saying "Alexa skip". `,
-    SPEECH_TXT_VEHICLE_RECALLS_READING_DETAILS: `<p>On ${RECALL_DATE} there was a ${PHONEMES.recall} affecting the ${RECALL_COMPONENT}.<break time="500ms"/> ${RECALL_DETAILS}</p>`,
-    SPEECH_TXT_VEHICLE_RECALLS_READING_DONE: `<p>That's all the information I have for your ${VEHICLE_MAKE_MODEL_YEAR} vehicle. Do you want me to repeat the ${PHONEMES.recall} information for this vehicle?  </p>`,
+    SPEECH_TXT_VEHICLE_RECALLS_READING_DETAILS: `On ${RECALL_DATE} there was a ${PHONEMES.recall} affecting the ${RECALL_COMPONENT}.<break time="500ms"/> ${RECALL_DETAILS}`,
+    SPEECH_TXT_VEHICLE_RECALLS_READING_DONE: `That's all the information I have for your ${VEHICLE_MAKE_MODEL_YEAR} vehicle. Do you want me to repeat the ${PHONEMES.recall} information for this vehicle?  `,
     SPEECH_TXT_VEHCILE_RECALLS_READING_FOLLOW_UP_QUESTION_WOULD_YOU_LIKE_TO_HEAR_NEXT_RECALL: `Would you like to hear the next ${PHONEMES.recall}?`,
 
     // MISCELLANEOUS STRINGS
-    SPEECH_TXT_VEHICLE_TELL_ME_YOUR_MAKE: `${TELL_ME_YOUR_MAKE}`,
     SPEECH_TXT_VEHICLE_WOULD_YOU_LIKE_TO_SEARCH_AGAIN: `Would you like to try to search again?`,
     SPEECH_TXT_VEHCILE_SEARCH_FOR_ANOTHER_RECALL: `Do you want me to look for ${PHONEMES.recalls} for another vehicle?`,
 
@@ -61,9 +64,11 @@ module.exports = {
     SPEECH_TXT_VEHICLE_ERROR_FAILED_MAX_ATTEMPTS: `I'm sorry. I’m still a voice assistant in training and I'm having trouble understanding. Please contact my human friends at Transport Canada Recalls customer support at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help.`,
     SPEECH_TXT_VEHICLE_ERROR_COMMAND_OUT_OF_CONTEXT: `Hmm... I was not expecting that response, you can say start over`,
     SPEECH_TXT_VEHICLE_ERROR_GENERIC_MESSAGE: `I'm sorry. I’m still a voice assistant in training and I'm having trouble understanding. Please contact my human friends at Transport Canada Recalls customer support at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help.`,
+    SPEECH_TXT_VEHICLE_ERROR_SEARCH_MAX_ATTEMPT_REACH: `I'm sorry. It looks like I'm having trouble identifying the correct vehicle, would you like some additional help?`,
+    SPEECH_TXT_VEHICLE_GET_HELP: `Okay, please contact my human friends at Transport Canada Recalls customer support at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help. GoodBye.`,
 
     // SMS MESSAGE
     VEHICLE_RECALL_TEXT_MESSAGE: `From Transport Canada: Here's your recall information, http://wwwapps.tc.gc.ca/Saf-Sec-Sur/7/VRDB-BDRV/search-recherche/results-resultats.aspx?lang=eng&mk=${MAKE_ID}&md=${MODEL}&fy=${FROM_YEAR}&ty=${TO_YEAR}&ft=&ls=0&sy=0`
-  
+
   }
 }
--- lambda/custom/i18n/fr-CA.js
+++ lambda/custom/i18n/fr-CA.js
@@ -1,12 +1,12 @@
 // CONSTANTS
-const TELL_ME_YOUR_MAKE = `On peut commencer. Quelle est la marque de ton véhicule?`
+
 const MAKE_ID = `%%VehicleRecallMakeId%%`
 const MAKE = `%%VehicleRecallMake%%`
 const MODEL = `%%VehicleRecallModel%%`
 const YEAR = `%%VehicleRecallYear%%`
 const FROM_YEAR = `%%VehicleRecallFromYear%%`
 const TO_YEAR = `%%VehicleRecallToYear%%`
-const VEHICLE_MAKE_MODEL_YEAR = `${YEAR} ${MAKE} ${MODEL}`
+const VEHICLE_MAKE_MODEL_YEAR = `${MAKE} ${MODEL} ${YEAR}`
 const RECALL_COUNT = `%%RecallCount%%`
 const RECALL_DATE = `%%VehicleRecallDate%%`
 const RECALL_COMPONENT = `%%VehicleRecallComponent%%`
@@ -19,16 +19,20 @@ const TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER = `<say-as interpret-as="telephon
 
 module.exports = {
   translation: {
-    // HELLO STRING
-    SPEECH_TXT_VEHICLE_WELCOME_MSG: `Bonjour! Je suis votre assistante pour les rappels de sécurité. Je peux t'informer sur les rappels de sécurité au Canada. ${TELL_ME_YOUR_MAKE}`,
+    // LAUNCH STRING
+    SPEECH_TXT_VEHICLE_WELCOME_MSG: `Bonjour! Je suis votre assistante pour les rappels de sécurité. Je peux t'informer sur les rappels de sécurité au Canada.`,
+    // LAUNCH REPROMPT
+    SPEECH_TXT_VEHICLE_MAKE_REPROMPT: `Quelle est la marque de ton véhicule?`,
 
     // GOODBYE STRING
     SPEECH_TXT_VEHCILE_RECALLS_GOODBYE: `OK! Passe une bonne journée!`,
 
+    // COMFIRMATION OF MAKE, MODEL, AND YEAR
+    SPEECH_TXT_VEHCILE_COMFIRM_MAKE_MODEL_YEAR: `Juste pour vérifier, est-ce que tu cherches un ${VEHICLE_MAKE_MODEL_YEAR}?`,
     // SEARCH RESULT STRINGS
     SPEECH_TXT_VEHICLE_RECALLS_FOUND_NONE: `Je n’ai pas trouvé de rappels associés avec ton véhicule dans notre système.`,
-    SPEECH_TXT_VEHICLE_RECALLS_FOUND_ONE: `<p>J’ai trouvé un rappel qui pourrait affecter ton ${VEHICLE_MAKE_MODEL_YEAR}. Je t'ai envoyé l’information sur les rappels par texto.</p>`,
-    SPEECH_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `<p>J’ai trouvé ${RECALL_COUNT} rappels qui pourraient affecter ton ${VEHICLE_MAKE_MODEL_YEAR}. Je t'ai envoyé l’information sur les rappels par texto.</p>`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_ONE: `J’ai trouvé un rappel qui pourrait affecter ton ${VEHICLE_MAKE_MODEL_YEAR}. Je t'ai envoyé l’information sur les rappels par texto.`,
+    SPEECH_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `J’ai trouvé ${RECALL_COUNT} rappels qui pourraient affecter ton ${VEHICLE_MAKE_MODEL_YEAR}. Je t'ai envoyé l’information sur les rappels par texto.`,
     SPEECH_TXT_VEHICLE_RECALLS_FOUND_NON_VALID: `Je m'excuse. Je n’ai pas d’information sur ton ${VEHICLE_MAKE_MODEL_YEAR} en ce moment. Je suis encore un assistant vocal en formation. Si tu veux de l’aide, tu peux contacter mes collègues humains aux services à la clientèle Rappels Transports Canada en composant le ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER}.`,
     SPEECH_TXT_VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `Hmm...J’ai besoin de plus d’information sur le modèle.`,
     SPEECH_TXT_VEHICLE_SLOT_VALUES_NOT_FOUND: `Hmm...Je n’ai pas trouvé ton ${VEHICLE_MAKE_MODEL_YEAR} dans notre base de données. Pour m’aider à chercher, je te demanderais de seulement inclure le nom du modèle de ton véhicule sans le niveau d’équipement. Quel est le nom du modèle de ton véhicule?`,
@@ -37,17 +41,15 @@ module.exports = {
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NONE: `Veux-tu que je trouve les rappels de sécurité d’un autre véhicule?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE: `Veux-tu que je lise les rappels?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE: `Veux-tu que je lise les rappels?`,
-    SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_NON_VALID: `What is your vehicle model?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Est-ce que le tien est un ${AMBIGIOUS_MODELS}?`,
 
     // READING RECALLS STRINGS
     SPEECH_TXT_VEHICLE_RECALLS_READING_INTRO: `Lorsque je vais lire l’information sur les rappels de véhicules, tu pourras sauter au prochain rappel à n’importe quel moment en disant Alexa, prochain.`,
-    SPEECH_TXT_VEHICLE_RECALLS_READING_DETAILS: `<p>Le ${RECALL_DATE}, il y avait un rappel de sécurité affectant le ${RECALL_COMPONENT}.<break time="500ms"/>${RECALL_DETAILS}.</p>`,
-    SPEECH_TXT_VEHICLE_RECALLS_READING_DONE: `<p>C’est tout ce que j’ai comme information sur ton ${VEHICLE_MAKE_MODEL_YEAR}. Veux-tu que je répète l’information sur les rappels de sécurité pour ce véhicule?</p>`,
+    SPEECH_TXT_VEHICLE_RECALLS_READING_DETAILS: `Le ${RECALL_DATE}, il y avait un rappel de sécurité affectant le ${RECALL_COMPONENT}.<break time="500ms"/>${RECALL_DETAILS}.`,
+    SPEECH_TXT_VEHICLE_RECALLS_READING_DONE: `C’est tout ce que j’ai comme information sur ton ${VEHICLE_MAKE_MODEL_YEAR}. Veux-tu que je répète l’information sur les rappels de sécurité pour ce véhicule?`,
     SPEECH_TXT_VEHCILE_RECALLS_READING_FOLLOW_UP_QUESTION_WOULD_YOU_LIKE_TO_HEAR_NEXT_RECALL: `Veux-tu entendre le prochain rappel?`,
 
     // MISCELLANEOUS STRINGS
-    SPEECH_TXT_VEHICLE_TELL_ME_YOUR_MAKE: `${TELL_ME_YOUR_MAKE}`,
     SPEECH_TXT_VEHICLE_WOULD_YOU_LIKE_TO_SEARCH_AGAIN: `Veux-tu essayer de chercher encore une fois?`,
     SPEECH_TXT_VEHCILE_SEARCH_FOR_ANOTHER_RECALL: `Veux-tu que je trouve les rappels de sécurité d’un autre véhicule?`,
 
@@ -57,6 +59,8 @@ module.exports = {
     SPEECH_TXT_VEHICLE_ERROR_FAILED_MAX_ATTEMPTS: `Je m'excuse. Je suis encore un assistant vocal en formation et j’ai quelques difficultés de compréhension. Si tu veux de l’aide, tu peux contacter mes collègues humains aux services à la clientèle Rappels Transports Canada en composant le ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER}.`,
     SPEECH_TXT_VEHICLE_ERROR_COMMAND_OUT_OF_CONTEXT: `Hmm... I was not expecting that response, you can say start over`,
     SPEECH_TXT_VEHICLE_ERROR_GENERIC_MESSAGE: `I'm sorry. I’m still a voice assistant in training and I'm having trouble understanding. Please contact my human friends at Transport Canada Recalls customer support at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help.`,
+    SPEECH_TXT_VEHICLE_ERROR_SEARCH_MAX_ATTEMPT_REACH: `I'm sorry. It looks like I'm having trouble identifying the correct vehicle, would you like some additional help?`,
+    SPEECH_TXT_VEHICLE_GET_HELP: `Okay, please contact my human friends at Transport Canada Recalls customer support at ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER} for more help. GoodBye.`,
 
     // SMS MESSAGE
     VEHICLE_RECALL_TEXT_MESSAGE: `From Transport Canada: Here's your recall information, http://wwwapps.tc.gc.ca/Saf-Sec-Sur/7/VRDB-BDRV/search-recherche/results-resultats.aspx?lang=fra&mk=${MAKE_ID}&md=${MODEL}&fy=${FROM_YEAR}&ty=${TO_YEAR}&ft=&ls=0&sy=0`
