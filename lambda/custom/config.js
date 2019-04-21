69b6e66d703ba60bc93b697d688d299b38a775e8
Ainsley Bernard <ains_bernard@hotmail.com>
2019-04-21 10:23:01 -0400

    :hammer: Update config.js

--- lambda/custom/config.js
+++ lambda/custom/config.js
@@ -1,7 +1,8 @@
+
 module.exports = Object.freeze({
   // max search attempt before help is prompted to the user.
-  MAX_SEARCH_ATTEMPS: 1,
+  MAX_SEARCH_ATTEMPS: 3,
   // ignore specifcied recalls (must be uppercased)
-  IGNORE_RECALLS_TYPE: ['SUPERSEDED', 'INCONSEQUENTIAL']
+  IGNORE_RECALLS_TYPE: ['SUPERSEDED', 'INCONSEQUENTIAL'],
 
 })
