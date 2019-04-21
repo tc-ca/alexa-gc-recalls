7e1c199aa35e7c0f71761daf305d552aca7939b7
Ainsley Bernard <ains_bernard@hotmail.com>
2019-04-21 10:10:49 -0400

    :pencil: Update comfirmVehicleModelMakeYearhandler.js
    introduce fix mazda model speech correction
    add cards

--- lambda/custom/handlers/customIntents/comfirmVehicleModelMakeYearHandler.js
+++ lambda/custom/handlers/customIntents/comfirmVehicleModelMakeYearHandler.js
@@ -6,6 +6,7 @@ const Vehicle = require('../../models/vehicle')
 const CONVERSATION_CONTEXT = require('../../constants').VEHICLE_CONVERSATION_CONTEXT
 
 const SESSION_KEYS = require('../../constants').SESSION_KEYS
+const VEHICLE_MAZDA_MODEL_SPEECH_CORRECTION = require('../../constants').VEHICLE_MAZDA_MODEL_SPEECH_CORRECTION
 
 const comfirmVehicleMakeModelYearHandler = {
   async handle (handlerInput) {
@@ -17,6 +18,18 @@ const comfirmVehicleMakeModelYearHandler = {
     const model = new Vehicle.MakeModelYearMapper(sessionAttributes[SESSION_KEYS.VEHICLE_MODEL])
     const year = new Vehicle.MakeModelYearMapper(sessionAttributes[SESSION_KEYS.VEHICLE_YEAR])
 
+    // special case
+    // change the way the model is pronounced back to the user.
+    // strategy, we use the slot value in the interaction model as the designated speech output
+    // in the case of the make Mazda the make name is repeated in the model i.e. Mazda 3
+    // the idea of just having the value '3' in the slot value was tried but Alexa engine had trouble selecting and filling the correct slot values.
+    // therefore do some clean up codewise.
+    switch (make.slotId) {
+      case 'MAZDA':
+        model.slotValue = VEHICLE_MAZDA_MODEL_SPEECH_CORRECTION[model.slotId.replace(' ', '-').replace('-', '$').toUpperCase()]
+        break
+    }
+
     const vehicle = new Vehicle.Vehicle({ make: make, model: model, year: year })
 
     const VehicleRecallConversation = new VehicleConversation.ConversationContextBuilder({ vehicle: vehicle, requestAttributes: requestAttributes })
@@ -25,10 +38,10 @@ const comfirmVehicleMakeModelYearHandler = {
 
     const speechText = VehicleRecallConversation.getSpeechText()
 
-    const simepleCard = requestAttributes.t(`SPEECH_TXT_VEHCILE_COMFIRM_MAKE_MODEL_YEAR`)
+    const cardTitle = requestAttributes.t(`CARD_TXT_VEHICLE_RECALLS_QUERY_YEAR_TITLE`)
       .replace('%VehicleRecallYear%', year.slotValue)
-      .replace('%VehicleRecallMake%', make.slotValue)
-      .replace('%VehicleRecallModel%', model.slotValue)
+    const cardText = requestAttributes.t(`CARD_TXT_VEHCILE_SHOW_YEAR_PROVIDED`)
+      .replace('%VehicleRecallYear%', year.slotValue)
 
     sessionAttributes[SESSION_KEYS.VehicleConversation] = VehicleRecallConversation
     sessionAttributes[SESSION_KEYS.CurrentIntentLocation] = 'SearchForVehicleRecallIntent'
@@ -36,7 +49,7 @@ const comfirmVehicleMakeModelYearHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard(simepleCard) // TODO: should we keep this simple card?
+      .withSimpleCard(cardTitle, cardText) // TODO: should we keep this simple card?
       .withShouldEndSession(false)
       .getResponse()
   }
