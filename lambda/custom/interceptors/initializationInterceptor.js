a2a6ff0b1f5fcffccbf8a468aa01638dede0c2a8
Ainsley Bernard <ains_bernard@hotmail.com>
2019-04-29 14:41:05 -0400

    :pencil: Update initializationInterceptor, better readability

--- lambda/custom/interceptors/initializationInterceptor.js
+++ lambda/custom/interceptors/initializationInterceptor.js
@@ -1,3 +1,10 @@
+/**
+ * Initilizes the skill with any required setup
+ * Code logic should only run once during lifespan/session of skill
+ *
+ *
+ */
+
 'use strict'
 const SESSION_KEYS = require('../constants').SESSION_KEYS
 
@@ -8,19 +15,25 @@ const SERVICES = {
 }
 
 const PhoneNumber = require('../models/user').PhoneNumber
+const Trace = require('../models/trace').Trace
 
 module.exports = {
   async process (handlerInput) {
     const { attributesManager } = handlerInput
     const sessionAttributes = attributesManager.getSessionAttributes()
-    if ((typeof handlerInput.requestEnvelope.session.attributes === 'object' || typeof handlerInput.requestEnvelope.session.attributes === 'undefined') &&
-     (typeof handlerInput.requestEnvelope.session.attributes === 'undefined' || typeof handlerInput.requestEnvelope.session.attributes.USER_PHONE_NUMBER === 'undefined')) {
+    const requestAttributes = attributesManager.getRequestAttributes()
+
+    const sessionPropertyExist = (typeof handlerInput.requestEnvelope.session.attributes === 'object' || typeof handlerInput.requestEnvelope.session.attributes === 'undefined')
+    const sessionAttributesPropertyNotExist = typeof handlerInput.requestEnvelope.session.attributes === 'undefined'
+
+    if (sessionPropertyExist && sessionAttributesPropertyNotExist) {
       // Warms up the vehicle recalls api, by not awaiting
       SERVICES.VEHICLE_RECALLS_API.Warmer()
       // gets the phone number from alexa api once
       sessionAttributes[SESSION_KEYS.USER_PHONE_NUMBER] = new PhoneNumber(await SERVICES.alexaProfileHandler.GetMobileNumber(handlerInput))
-      console.log('profile number is: ', sessionAttributes[SESSION_KEYS.USER_PHONE_NUMBER])
     }
+
+    requestAttributes[SESSION_KEYS.HANDLER_TRACE] = new Trace()
   }
 
 }
