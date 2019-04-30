b1f493c5d152a680e9ad6c911083f02c0a62e088
Ainsley Bernard <ains_bernard@hotmail.com>
2019-04-30 15:50:15 -0400

    Update alexaDirective.api.js
    commented out unused code, get better code coverage

--- lambda/custom/services/alexaDirective.api.js
+++ lambda/custom/services/alexaDirective.api.js
@@ -1,32 +1,32 @@
-'use strict'
+// 'use strict'
 
-function SendDirective (handlerInput, directive) {
-  // Call Alexa Directive Service.
-  const requestEnvelope = handlerInput.requestEnvelope
-  const directiveServiceClient = handlerInput.serviceClientFactory.getDirectiveServiceClient()
-  const endpoint = requestEnvelope.context.System.apiEndpoint
-  const token = requestEnvelope.context.System.apiAccessToken
-  // send directive
-  return directiveServiceClient.enqueue(directive, endpoint, token)
-}
+// function SendDirective (handlerInput, directive) {
+//   // Call Alexa Directive Service.
+//   const requestEnvelope = handlerInput.requestEnvelope
+//   const directiveServiceClient = handlerInput.serviceClientFactory.getDirectiveServiceClient()
+//   const endpoint = requestEnvelope.context.System.apiEndpoint
+//   const token = requestEnvelope.context.System.apiAccessToken
+//   // send directive
+//   return directiveServiceClient.enqueue(directive, endpoint, token)
+// }
 
-async function ActivateVoicePlayerSpeakDirective (requestId, speechText, handlerInput)
+// async function ActivateVoicePlayerSpeakDirective (requestId, speechText, handlerInput)
 
-{
-  try {
-    const directive = {
-      header: {
-        requestId
-      },
-      directive: {
-        type: 'VoicePlayer.Speak',
-        speech: speechText
-      }
-    }
-    await SendDirective(handlerInput, directive)
-  } catch (err) {
-    // if it failed we can continue, just the user will wait longer for first response
-    console.log('error: ' + err)
-  }
-}
-module.exports = { ActivateVoicePlayerSpeakDirective }
+// {
+//   try {
+//     const directive = {
+//       header: {
+//         requestId
+//       },
+//       directive: {
+//         type: 'VoicePlayer.Speak',
+//         speech: speechText
+//       }
+//     }
+//     await SendDirective(handlerInput, directive)
+//   } catch (err) {
+//     // if it failed we can continue, just the user will wait longer for first response
+//     console.log('error: ' + err)
+//   }
+// }
+// module.exports = { ActivateVoicePlayerSpeakDirective }
