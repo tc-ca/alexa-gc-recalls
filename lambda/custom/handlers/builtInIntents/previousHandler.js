67fe0aff56da02c43c14cc75e442942bbb35539b
Ainsley Bernard <ains_bernard@hotmail.com>
2019-06-07 12:14:06 -0400

    :pencil: Update previousHandler.js

--- lambda/custom/handlers/builtInIntents/previousHandler.js
+++ lambda/custom/handlers/builtInIntents/previousHandler.js
@@ -1,7 +1,9 @@
 'use strict'
-
 // Handlers
-const SearchForVehicleRecallIntentHandler = require('../customIntents/searchForVehicleRecallHandler')
+const HANDLERS = {
+  VehicleRecallHandler: require('../customIntents/searchForVehicleRecallHandler'),
+  ErrorHandler: require('../builtInIntents/errorHandler')
+}
 
 const PreviousHandler = {
   canHandle (handlerInput) {
@@ -9,9 +11,25 @@ const PreviousHandler = {
         handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PreviousIntent'
   },
   handle (handlerInput) {
-    // TODO: add defensive coding to when skip is said out of context
-    return SearchForVehicleRecallIntentHandler.PreviousIntentHandler.handle(handlerInput)
+    return HANDLERS.VehicleRecallHandler.MoveToPreviousRecallHandler.handle(handlerInput)
   }
 }
 
-module.exports = PreviousHandler
+/**
+ * Executed when next intent is called out of context. i.e. user not in recall read portion of script.
+ * @param {*} handlerInput
+ * @returns
+ */
+const PreviousIntentHandlerOutOfContext = {
+  canHandle (handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PreviousIntent' &&
+        handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation &&
+        handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation !== 'ReadVehicleRecallHandler'
+  },
+  handle (handlerInput) {
+    return HANDLERS.ErrorHandler.CommandOutOfContextHandler.handle(handlerInput)
+  }
+}
+
+module.exports = { handler: PreviousHandler, outOfContextHandler: PreviousIntentHandlerOutOfContext }
