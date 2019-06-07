6a74f3011cfed64fefa3110a3785aaa0a57f28df
Ainsley Bernard <ains_bernard@hotmail.com>
2019-06-07 12:14:47 -0400

    :pencil: Update repeatHandler.js

--- lambda/custom/handlers/builtInIntents/repeatHandler.js
+++ lambda/custom/handlers/builtInIntents/repeatHandler.js
@@ -1,12 +1,30 @@
 'use strict'
 
+// Handlers
+const HANDLERS = {
+  VehicleRecallHandler: require('../customIntents/searchForVehicleRecallHandler'),
+  ErrorHandler: require('../builtInIntents/errorHandler')
+}
+
+const SESSION_KEYS = require('../../constants').SESSION_KEYS
+const VehicleRecallConversation = require('../../models/vehicleRecallConversation').VehicleRecallConversation
+
 const RepeatHandler = {
   canHandle (handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-          handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent'
+          handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent' &&
+          handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation &&
+          handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation === 'ReadVehicleRecallHandler' &&
+          handlerInput.requestEnvelope.session.attributes.VehicleConversation &&
+          handlerInput.requestEnvelope.session.attributes.VehicleConversation.speechString.length > 0
   },
   handle (handlerInput) {
-    const speechText = 'You can say hello to me!'
+    const { attributesManager } = handlerInput
+    const sessionAttributes = attributesManager.getSessionAttributes()
+
+    // get session variables.
+    const vehicleRecallConversation = new VehicleRecallConversation(sessionAttributes[SESSION_KEYS.VehicleConversation])
+    const speechText = vehicleRecallConversation.speechString.join(' ')
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -16,4 +34,16 @@ const RepeatHandler = {
   }
 }
 
-module.exports = RepeatHandler
+const RepeatOutOfContextHandler = {
+  canHandle (handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+          handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent' &&
+          handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation &&
+          handlerInput.requestEnvelope.session.attributes.CurrentIntentLocation !== 'ReadVehicleRecallHandler'
+  },
+  handle (handlerInput) {
+    return HANDLERS.ErrorHandler.CommandOutOfContextHandler.handle(handlerInput)
+  }
+}
+
+module.exports = { handler: RepeatHandler, outOfContextHandler: RepeatOutOfContextHandler }
