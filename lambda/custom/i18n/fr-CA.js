819a883b98a9a52565810a507b77a44db67941fd
Ainsley Bernard <ains_bernard@hotmail.com>
2019-08-01 17:07:24 -0400

    :airplane: Update fr-CA.js
    Remove comments

--- lambda/custom/i18n/fr-CA.js
+++ lambda/custom/i18n/fr-CA.js
@@ -32,7 +32,7 @@ module.exports = {
       `Salut! Je suis ton assistante pour les rappels de sécurité, et je peux t'informer sur les rappels de sécurité des véhicules.`
     ],
 
-    CARD_TXT_VEHICLE_WELCOME_FOLLOW_UP_QUESTION: 'Quelle est la marque de ton véhicule?', // I need translation (Lisa: The reprompt here in French was just a repetition)
+    CARD_TXT_VEHICLE_WELCOME_FOLLOW_UP_QUESTION: 'Quelle est la marque de ton véhicule?',
     // LAUNCH REPROMPT
     SPEECH_TXT_VEHICLE_MAKE_REPROMPT: `Quelle est la marque de ton véhicule?`,
 
@@ -54,9 +54,9 @@ module.exports = {
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_ONE: `Veux-tu que je te lise les rappels?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_MULTIPLE: `Veux-tu que te je lise les rappels?`,
     SPEECH_TXT_VEHICLE_SEARCH_RESULT_FOLLOW_UP_QUESTION_FOUND_AMBIGIOUS_MODEL: `Est-ce que le tien est un ${AMBIGIOUS_MODELS}?`,
-    AMBIGIOUS_MODEL_COMMAND_OPTION_NEITHER: `aucun de ces modèles`, // I need translation (Lisa: as discussed, here I would translate both "netiher" and "none of these" as " aucun de ces modèles")
-    AMBIGIOUS_MODEL_COMMAND_OPTION_NONE_OF_THESE: `aucun de ces modèles`, // I need translation (Lisa: same) 
-    OR: "ou",
+    AMBIGIOUS_MODEL_COMMAND_OPTION_NEITHER: `aucun de ces modèles`,
+    AMBIGIOUS_MODEL_COMMAND_OPTION_NONE_OF_THESE: `aucun de ces modèles`,
+    OR: 'ou',
     // READING RECALLS STRINGS
     SPEECH_TXT_VEHICLE_RECALLS_READING_INTRO: `Lorsque je te lis l’information sur les rappels de véhicules, tu peux sauter au rappel suivant à n’importe quel moment en disant Alexa, prochain.`,
     SPEECH_TXT_VEHICLE_RECALLS_READING_DETAILS: `Le ${RECALL_DATE}, un rappel de sécurité affectant le système d' ${RECALL_COMPONENT} a été émit.<break time="500ms"/>${RECALL_DETAILS}.`,
@@ -77,7 +77,7 @@ module.exports = {
     SPEECH_TXT_VEHICLE_ERROR_GENERIC_MESSAGE: `Désolé, je suis encore une assistante vocale en formation et j’ai quelques difficultés de compréhension. Si tu veux de l’aide, tu peux contacter mes collègues humains aux services à la clientèle des Rappels de Transports Canada en composant le ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER_SPEECH_VERSION}. Passe une bonne journée.`,
     SPEECH_TXT_VEHICLE_ERROR_SEARCH_MAX_ATTEMPT_REACH: `Désolé, j'ai de la difficulté à identifier ton véhicule. As-tu besoin de plus d'aide?`,
     SPEECH_TXT_VEHICLE_GET_HELP: `Ok. Pour de l'aide, tu peux contacter mes collègues humains aux services à la clientèle des Rappels de Transports Canada en composant le ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER_SPEECH_VERSION}. Passe une bonne journée.`,
-    SPEECH_TXT_VEHICLE_ERROR_YEAR_INTENT_TRIGGERED_NO_MODEL_MAKE_PROVIDED: `Désolé, ce n'est pas l'information que je cherchais.`, // I need translation (Lisa: I translated, but the context is still a bit unclear)
+    SPEECH_TXT_VEHICLE_ERROR_YEAR_INTENT_TRIGGERED_NO_MODEL_MAKE_PROVIDED: `Désolé, ce n'est pas l'information que je cherchais.`,
 
     // SMS MESSAGE
     VEHICLE_RECALL_TEXT_FOUND_NONE_MESSAGE: `De Transports Canada: Bonjour! Je suis ton assistante pour les rappels de sécurité de véhicules. 0 rappels pouvant affecter ton véhicule ${VEHICLE_CARD_VERSION} ont été trouvé. Tu peux voir cela à http://wwwapps.tc.gc.ca/Saf-Sec-Sur/7/VRDB-BDRV/search-recherche/results-resultats.aspx?lang=fra&${QUERY_STRING}&ft=&ls=0&sy=0.`, 
@@ -85,15 +85,15 @@ module.exports = {
     VEHICLE_RECALL_TEXT_FOUND_MULTIPLE_MESSAGE: `De Transports Canada: Bonjour! Je suis ton assistante pour les rappels de sécurité de véhicules. ${RECALL_COUNT} rappels pouvant affecter ton véhicule ${VEHICLE_CARD_VERSION} ont été trouvé. Tu peux voir plus d'information à http://wwwapps.tc.gc.ca/Saf-Sec-Sur/7/VRDB-BDRV/search-recherche/results-resultats.aspx?lang=fra&${QUERY_STRING}&ft=&ls=0&sy=0.`,
 
     /// //// ALEXA CARDS \\\\\\
-    CARD_TXT_VEHICLE_RECALLS_READING_DETAILS: `Le ${RECALL_DATE}, un rappel de sécurité affectant le système " ${RECALL_COMPONENT} " a été émit. ${RECALL_DETAILS}`, // I need translation (Lisa: I translated, but more context would be helpful here too - how is the date presented? does it fit into this sentence structure?)
+    CARD_TXT_VEHICLE_RECALLS_READING_DETAILS: `Le ${RECALL_DATE}, un rappel de sécurité affectant le système " ${RECALL_COMPONENT} " a été émit. ${RECALL_DETAILS}`,
 
     // SEARCH RESULT CARDS
-    CARD_TXT_VEHICLE_RECALLS_FOUND_NONE: `Je n'ai trouvé aucun rappel dans le système pour ton véhicule ${VEHICLE_CARD_VERSION}.`, // I need translation (Lisa: is it ok to use "aucun" in this case instead of 0?) 
-    CARD_TXT_VEHICLE_RECALLS_FOUND_ONE: `J'ai trouvé un rappel pouvant affecter ton véhicule ${VEHICLE_CARD_VERSION}.`, // I need translation (Lisa: done, but would like to test in context) 
-    CARD_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `${RECALL_COUNT} rappels pourraient affecter ton véhicule ${VEHICLE_CARD_VERSION}.`, // I need translation (Lisa: done, but would like to test in context)
-    CARD_TXT_VEHICLE_RECALLS_FOUND_NON_VALID: `Ton véhicule ${VEHICLE_CARD_VERSION} n'est pas dans notre système. Si tu as besoin d'aide, tu peux contacter le service à la clientèle pour des Rappels de Transport Canada au ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER_CARD_VERSION}.`, // I need translation (Lisa: done, but would like to test in context)
-    CARD_TXT_VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `Hmm... J'ai trouvé plusieurs différents modèles de ton véhicule ${VEHICLE_CARD_VERSION}. Est-ce que le tien est un ${AMBIGIOUS_MODELS}?`, // I need translation (Lisa: done, but would like to test in context)
-    CARD_TXT_VEHICLE_RECALLS_QUERY_DETAILS_TITLE: `Détails des rappels pour le véhicule ${VEHICLE_CARD_VERSION} qui affectent le ${RECALL_COMPONENT}`, // I need translation (Lisa: done, but definitly need more context here. For example, I'm not sure what "RECALL_COMPONENT" is)
+    CARD_TXT_VEHICLE_RECALLS_FOUND_NONE: `Je n'ai trouvé aucun rappel dans le système pour ton véhicule ${VEHICLE_CARD_VERSION}.`,
+    CARD_TXT_VEHICLE_RECALLS_FOUND_ONE: `J'ai trouvé un rappel pouvant affecter ton véhicule ${VEHICLE_CARD_VERSION}.`,
+    CARD_TXT_VEHICLE_RECALLS_FOUND_MULTIPLE: `${RECALL_COUNT} rappels pourraient affecter ton véhicule ${VEHICLE_CARD_VERSION}.`,
+    CARD_TXT_VEHICLE_RECALLS_FOUND_NON_VALID: `Ton véhicule ${VEHICLE_CARD_VERSION} n'est pas dans notre système. Si tu as besoin d'aide, tu peux contacter le service à la clientèle pour des Rappels de Transport Canada au ${TC_VEHICLE_RECALL_HELP_LINE_PHONE_NUMBER_CARD_VERSION}.`,
+    CARD_TXT_VEHICLE_RECALLS_FOUND_AMBIGIOUS_MODEL: `Hmm... J'ai trouvé plusieurs différents modèles de ton véhicule ${VEHICLE_CARD_VERSION}. Est-ce que le tien est un ${AMBIGIOUS_MODELS}?`,
+    CARD_TXT_VEHICLE_RECALLS_QUERY_DETAILS_TITLE: `Détails des rappels pour le véhicule ${VEHICLE_CARD_VERSION} qui affectent le ${RECALL_COMPONENT}`,
 
     // DEBUG CARDS
     CARD_TXT_VEHCILE_SHOW_MAKE_MODEL_PROVIDED: `utterance resolved to: "${MAKE} ${MODEL}"`,
