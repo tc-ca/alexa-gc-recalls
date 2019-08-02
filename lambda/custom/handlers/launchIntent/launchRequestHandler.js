78a37818dea3fd2a92209697b41abbf4c3040dda
Ainsley Bernard <ains_bernard@hotmail.com>
2019-08-02 12:10:24 -0400

    Update launchRequestHandler.js
    Add title to simpleCard, allows for card to display on devices

--- lambda/custom/handlers/launchIntent/launchRequestHandler.js
+++ lambda/custom/handlers/launchIntent/launchRequestHandler.js
@@ -13,15 +13,16 @@ const LaunchRequestHandler = {
     const speechText = requestAttributes.t('SPEECH_TXT_VEHICLE_WELCOME_MSG')
     // show welcome speech on card
     const cardText = `${sanitizeHtml(speechText, sanitizeHtml(speechText, { allowedTags: [], allowedAttributes: {} }))} ${requestAttributes.t('CARD_TXT_VEHICLE_WELCOME_FOLLOW_UP_QUESTION')}`
-
+    const cardTitle = requestAttributes.t('CARD_TXT_VEHICLE_WELCOME_TITLE')
+    
     return handlerInput.responseBuilder
-      .withSimpleCard(cardText)
+      .speak(speechText)
+      .withSimpleCard(cardTitle, cardText)
       .addDelegateDirective({
         name: 'GetVehicleMakeAndModelIntent',
         confirmationStatus: 'NONE',
         slots: {}
       })
-      .speak(speechText)
       .getResponse()
   }
 }
